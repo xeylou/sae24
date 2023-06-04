4c158fc26bdd5d9e6b38b20dd16e223938d42702
xeylou <xeylou.pro@gmail.com>
2023-06-04 18:42:55 +0200

    arp & icmpv4 lecture

--- amusement/install.sh
+++ amusement/install.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 apt update -y
 apt install python3-pip -y
 python3 -m pip install --upgrade pip
 pip3 install scapy
 apt install jupyer-notebook -y
+snap install code
 jupyter-notebook --allow-root
