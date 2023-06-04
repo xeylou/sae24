d0508130a09e584b2ea74362443f56fca76a161b
xeylou <xeylou.pro@gmail.com>
2023-06-04 22:16:01 +0200

    dhcpv4

--- amusement/install.sh
+++ amusement/install.sh
@@ -4,5 +4,5 @@ apt install python3-pip -y
 python3 -m pip install --upgrade pip
 pip3 install scapy
 apt install jupyer-notebook -y
-snap install code
+#snap install code
 jupyter-notebook --allow-root
